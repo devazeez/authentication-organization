cded0a27845ec6ff809203bd3ad6f1ceb53ed50f
Azeez <l.adeosun.la@gmail.com>
2024-07-09 23:20:46 +0100

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import "dotenv/config"; // This should be at the very top of your main file
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import winston from "winston";
-import expressWinston from "express-winston";
-import { AuthRoute, OrganizationRoute } from "./modules";
-import serverless from "serverless-http"; // Import serverless at the top of the file
+import 'dotenv/config'; // This should be at the very top of your main file
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import winston from 'winston';
+import expressWinston from 'express-winston';
+import { AuthRoute, OrganizationRoute } from './modules';
+import serverless from 'serverless-http';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,11 +19,11 @@ app.use(
       winston.format.simple()
     ),
     meta: true, // log metadata (default: true)
-    msg: "HTTP {{req.method}} {{req.url}}",
+    msg: 'HTTP {{req.method}} {{req.url}}',
     expressFormat: true,
     colorize: false, // don't colorize the output
-    requestWhitelist: ["body"], // include request body in the logs
-    responseWhitelist: ["body"], // include response body in the logs
+    requestWhitelist: ['body'], // include request body in the logs
+    responseWhitelist: ['body'], // include response body in the logs
   })
 );
 
@@ -33,12 +33,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Routes
-app.use("/api", AuthRoute);
-app.use("/api", OrganizationRoute);
+app.use('/api', AuthRoute);
+app.use('/api', OrganizationRoute);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-// })
+// Only start the server if not in a serverless environment
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-export default serverless(app);
+// Export the app wrapped in serverless
+export const handler = serverless(app);
