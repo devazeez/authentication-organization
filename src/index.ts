7e089d08f35760438f014d3e678cd79b4206740a
Azeez <l.adeosun.la@gmail.com>
2024-07-09 23:12:02 +0100

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,31 @@
-import 'dotenv/config';  // This should be at the very top of your main file
+import "dotenv/config"; // This should be at the very top of your main file
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-// import winston from "winston";
+import winston from "winston";
 import expressWinston from "express-winston";
 import { AuthRoute, OrganizationRoute } from "./modules";
-import serverless from 'serverless-http'; // Import serverless at the top of the file
+import serverless from "serverless-http"; // Import serverless at the top of the file
 
-
-
-export const app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
 // LOGGING
-// app.use(
-//   expressWinston.logger({
-//     transports: [new winston.transports.Console()],
-//     format: winston.format.combine(
-//       winston.format.colorize(),
-//       winston.format.simple(),
-//     ),
-//     meta: true, // log metadata (default: true)
-//     msg: "HTTP {{req.method}} {{req.url}}",
-//     expressFormat: true,
-//     colorize: false, // don't colorize the output
-//     requestWhitelist: ["body"], // include request body in the logs
-//     responseWhitelist: ["body"], // include response body in the logs
-//   }),
-// );
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+    meta: true, // log metadata (default: true)
+    msg: "HTTP {{req.method}} {{req.url}}",
+    expressFormat: true,
+    colorize: false, // don't colorize the output
+    requestWhitelist: ["body"], // include request body in the logs
+    responseWhitelist: ["body"], // include response body in the logs
+  })
+);
 
 // Middleware
 app.use(bodyParser.json());
@@ -40,9 +36,9 @@ app.use(cors());
 app.use("/api", AuthRoute);
 app.use("/api", OrganizationRoute);
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // })
 
 export default serverless(app);
